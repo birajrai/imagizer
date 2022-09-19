2c715509b948b67cea9e8918de60f2f6fa263fa3
Biraj Rai <70630924+birajrai@users.noreply.github.com>
2022-09-19 11:51:14 +0000

    [+] Removed Authentication

--- index.js
+++ index.js
@@ -40,28 +40,10 @@ const upload = multer({
 
 // Main route
 app.get('/', (req, res) => {
-  const reject = () => {
-    res.setHeader('www-authenticate', 'Basic')
-    res.sendStatus(401)
-  }
-
-  const authorization = req.headers.authorization
-
-  if (!authorization) {
-    return reject()
-  }
-
-  const [username, password] = Buffer.from(authorization.replace('Basic ', ''), 'base64').toString().split(':')
-
-  if (!(username === 'admin' && password === 'admin')) {
-    return reject()
-  }
-
   res.render('index', {
     alert: null,
   });
 })
-
 // EJS route
 app.post('/', upload.single('image'), (req, res) => {
   const file = req.file;
