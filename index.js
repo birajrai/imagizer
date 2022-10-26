93826d72c12386d374d1b123803b6e5181ea0c7a
Biraj Rai <70630924+birajrai@users.noreply.github.com>
2022-10-26 15:24:40 +0545

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
-const fs = require('fs');
-const cors = require('cors');
-const multer = require('multer');
-const express = require('express');
-const config = require('./config.json');
+// Settings <3
+const config = require("./settings.js");
 
-const app = express();
-app.set('view engine', 'ejs');
-app.use('/images', express.static('images'));
+// Packages
+const fs = require("fs");
+const cors = require("cors");
+const multer = require("multer");
+const express = require("express");
+
+const app = (module.exports = express());
+app.set("view engine", "ejs");
+app.use("/", express.static("public"));
+app.use("/images", express.static("data"));
 
 app.use(cors());
 app.use(express.json());
@@ -14,17 +18,17 @@ app.use(express.urlencoded({ extended: true }));
 
 const upload = multer({
   storage: multer.diskStorage({
-    destination: `${__dirname}/images`,
+    destination: `${__dirname}/data`,
   }),
   limits: {
     fileSize: 104857600,
   },
-  fileFilter: function(req, file, callback) {
+  fileFilter: function (req, file, callback) {
     const supportedFiletypes = [
-      'image/gif',
-      'image/jpeg',
-      'image/jpg',
-      'image/png',
+      "image/gif",
+      "image/jpeg",
+      "image/jpg",
+      "image/png",
     ];
     if (supportedFiletypes.indexOf(file.mimetype) < 0) {
       if (!req.skippedFiles) {
@@ -38,39 +42,37 @@ const upload = multer({
   },
 });
 
-// Main route
-app.get('/', (req, res) => {
-  res.render('index', {
-    alert: null,
-  });
-})
-// EJS route
-app.post('/', upload.single('image'), (req, res) => {
+// Index Router
+app.get("/", (req, res) => {
+  res.render("index", { title: config.title, alert: null });
+});
+// Upload and Error Router :')
+app.post("/", upload.single("image"), (req, res) => {
   const file = req.file;
   if (file) {
-    const type = file.mimetype.split('/');
+    const type = file.mimetype.split("/");
     const name = randomString(10);
     fs.renameSync(
-      `${__dirname}/images/${file.filename}`,
-      `${__dirname}/images/${name}.${type[1]}`,
+      `${__dirname}/data/${file.filename}`,
+      `${__dirname}/data/${name}.${type[1]}`
     );
     res.redirect(`/${name}.${type[1]}`);
   } else {
-    res.render('index', {
-      alert: 'Only GIF, JPEG/JPG and PNG are allowed.',
+    res.render("index", {
+      alert: "Only GIF, JPEG/JPG and PNG are allowed.",
     });
   }
 });
 
-// Neppixel route
-app.post('/imagizer', upload.single('image'), (req, res) => {
+// Main Router
+app.post("/", upload.single("image"), (req, res) => {
   const file = req.file;
   if (file) {
-    const type = file.mimetype.split('/');
+    const type = file.mimetype.split("/");
     const name = randomString(10);
     fs.renameSync(
-      `${__dirname}/images/${file.filename}`,
-      `${__dirname}/images/${name}.${type[1]}`,
+      `${__dirname}/data/${file.filename}`,
+      `${__dirname}/data/${name}.${type[1]}`
     );
     res.send({ url: `${config.domain}/${name}.${type[1]}` });
   } else {
@@ -78,14 +80,15 @@ app.post('/imagizer', upload.single('image'), (req, res) => {
   }
 });
 
-// File display route
-app.get('/:filename', (req, res) => {
+// File Display Router
+app.get("/:filename", (req, res) => {
   try {
     const fileName = req.params.filename;
-    const fileStats = fs.statSync(`${__dirname}/images/${fileName}`);
+    const fileStats = fs.statSync(`${__dirname}/data/${fileName}`);
     const fileDate = `${fileStats.birthtime.toDateString()}, ${fileStats.birthtime.getHours()}:${fileStats.birthtime.getMinutes()}`;
     const fileSize = Math.round((fileStats.size / (1024 * 1024)) * 100) / 100;
-    res.render('viewer', {
+    res.render("viewer", {
+      title: config.title,
       fileName: fileName,
       fileSize: fileSize,
       fileDate: fileDate,
@@ -93,25 +96,26 @@ app.get('/:filename', (req, res) => {
       description: `I wasted ${fileSize}MB on ${fileDate}`,
     });
   } catch (error) {
-    res.redirect('/');
+    res.redirect("/");
   }
 });
 
-app.listen(config.port, () => {
-  console.log(`Imagizer server started on ${config.domain}:${config.port}`);
-});
+if (!module.parent) {
+  app.listen(config.port);
+  console.log(`Server started on  ${config.domain}:${config.port}`);
+}
 
 function randomHexColor() {
-  return '#' + Math.floor(Math.random() * 16777215).toString(16);
+  return "#" + Math.floor(Math.random() * 16777215).toString(16);
 }
 
 function randomString(length) {
   const randomChars =
-    'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
-  let result = '';
+    "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+  let result = "";
   for (let i = 0; i < length; i++) {
     result += randomChars.charAt(
-      Math.floor(Math.random() * randomChars.length),
+      Math.floor(Math.random() * randomChars.length)
     );
   }
   return result;
